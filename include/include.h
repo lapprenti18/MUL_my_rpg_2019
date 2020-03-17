acafc02fdb91b264ebcf830b7f6ca1c34720af3f
Maximilien Drules <maximilien@localhost.localdomain>
2020-03-17 09:55:12 +0100

    adding better .h management

---
+++ include/include.h
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2019
+** MUL_my_rpg_2019
+** File description:
+** include.h
+*/
+
+#ifndef INCLUDE_H_
+#define INCLUDE_H_
+
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/sysmacros.h>
+#include <signal.h>
+#include <SFML/Graphics.h>
+#include <SFML/Graphics/RenderWindow.h>
+#include <SFML/Graphics/Export.h>
+#include <SFML/Config.h>
+#include <SFML/System.h>
+#include <SFML/Window.h>
+#include <SFML/Audio.h>
+#include <math.h>
+#include <SFML/Graphics/Vertex.h>
+
+#endif /* INCLUDE_H */
